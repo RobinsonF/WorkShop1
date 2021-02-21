bd91d6cc3f1973cdaf42126c334b7848df03e8bc
RobinsonF <rgutierrezs@unbosque.edu.co>
2021-02-20 21:20:01 -0500

    Excepción id

--- src/co/edu/unbosque/fourpawscitizens/model/Manager.java
+++ src/co/edu/unbosque/fourpawscitizens/model/Manager.java
@@ -14,15 +14,15 @@ public class Manager {
     ArrayList<Pet> petsList = new ArrayList<Pet>();
 
     public Manager() {
-    this.uploadData();
-        for (int i = 0; i < petsList.size(); i++) {
-         System.out.println(petsList.get(i).getMicrochip() + "    " + petsList.get(i).getId());
-        }
-        System.out.println(petsList.size());
+        this.uploadData();
+        this.assignID();
+        //for (int i = 0; i < this.petsList.size(); i++) {
+         //   System.out.println(this.petsList.get(i).getId());
+        //}
     }
 
 
-    public void uploadData(){
+    public void uploadData() {
         BufferedReader bufferLectura = null;
         try {
             bufferLectura = new BufferedReader(new FileReader("C:\\Users\\Robinson\\WorkShop1\\src\\co\\edu\\unbosque\\fourpawscitizens\\pets-citizens.csv"));
@@ -32,45 +32,95 @@ public class Manager {
             while (linea != null) {
                 Pet pet = new Pet();
                 String[] campos = linea.split(";");
-                if(campos.length == 6 ){
+                if (campos.length == 6) {
                     try {
                         pet.setMicrochip(Long.parseLong(campos[0]));
-                    }catch (NumberFormatException e){
+                        pet.setSpecies(campos[1]);
+                        pet.setSex(campos[2]);
+                        pet.setSize(campos[3]);
+                        if (campos[4].equals("SI")) {
+                            campos[4] = "true";
+                        } else if (campos[4].equals("NO")) {
+                            campos[4] = "false";
+                        }
+                        pet.setPotentDangerous(Boolean.parseBoolean(campos[4]));
+                        pet.setNeighborhood(campos[5]);
+                        petsList.add(pet);
+                    } catch (NumberFormatException e) {
 
                     }
-                    
-                pet.setSpecies(campos[1]);
-                pet.setSex(campos[2]);
-                pet.setSize(campos[3]);
-                pet.setPotentDangerous(Boolean.parseBoolean(campos[4]));
-                pet.setNeighborhood(campos[5]);
-                    petsList.add(pet);
                 }
-
                 linea = bufferLectura.readLine();
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             // Cierro el buffer de lectura
             if (bufferLectura != null) {
                 try {
                     bufferLectura.close();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
 
-    public void assignID(){
+    public void assignID() {
+        int q = 0;
+        int s = 0;
+        String subCadenaSize = "";
+        String subCadenapotentDangerous = "";
         for (int i = 0; i < this.petsList.size(); i++) {
-            System.out.println(this.petsList.get(i).getSex() + "Hola");
+            String microChip = String.valueOf(this.petsList.get(i).getMicrochip());
+            String subCadenaMicroChip = microChip.substring(microChip.length() - 3, microChip.length());
+            String species = this.petsList.get(i).getSpecies();
+            String subCadenaSpecies = species.substring(0, 1);
+            String sex = this.petsList.get(i).getSex();
+            String subCadenaSex = sex.substring(0, 1);
+            String size = this.petsList.get(i).getSize();
+            if (size.equals("MINIATURA")) {
+                subCadenaSize = size.substring(0, 2);
+            } else {
+                subCadenaSize = size.substring(0, 1);
+            }
+            boolean potentDangerous = this.petsList.get(i).isPotentDangerous();
+            if (potentDangerous == true) {
+                subCadenapotentDangerous = "T";
+            } else if (potentDangerous == false) {
+                subCadenapotentDangerous = "F";
+            }
+            String id = subCadenaMicroChip + "-" + subCadenaSpecies + subCadenaSex + subCadenaSize + subCadenapotentDangerous + "-" + this.petsList.get(i).getNeighborhood();
+            this.petsList.get(i).setId(id);
+
+
         }
-        System.out.println("Hola");
+
+        for (int i = 0; i < this.petsList.size(); i++) {
+            q = 0;
+            for (int j = 0; j < this.petsList.size(); j++) {
+                if ( i != j && this.petsList.get(i).getId().equals(this.petsList.get(j).getId())) {
+                    String microChip = String.valueOf(this.petsList.get(i).getMicrochip());
+                    String subCadenaMicroChip = microChip.substring(microChip.length() - 4, microChip.length() - 3);
+                    String id = subCadenaMicroChip + this.petsList.get(j).getId();
+                    this.petsList.get(i).setId(id);
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < this.petsList.size(); i++) {
+            s = 0;
+            for (int j = 0; j < this.petsList.size(); j++) {
+                if ( i != j && this.petsList.get(i).getId().equals(this.petsList.get(j).getId())) {
+                s = s + 1;
+                System.out.println(s);
+                }
+                System.out.println(s);
+            }
+        }
 
     }
+
+
 }
